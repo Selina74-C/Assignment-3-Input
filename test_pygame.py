3141e86fc10ba70d87894325ffefe74ff4ac43d6
selina <selina>
2025-10-21 15:04:02 +0800

    Save work before revert

---
+++ test_pygame.py
@@ -0,0 +1,4 @@
+import pygame
+pygame.init()
+pygame.display.set_mode((400, 300))
+input("Press Enter to close...")
---
+++ test_pygame_driver.py
@@ -0,0 +1,5 @@
+import pygame
+pygame.init()
+print(pygame.display.get_driver())
+pygame.display.set_mode((400, 300))
+input("Press Enter to close...")
